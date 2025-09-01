796f2d6455e034545946ec81ffbe27f3dfa9d68d
miketester10 <cadenza91@gmail.com>
2025-09-01 06:15:19 +0200

    Refactor CurrentUser decorator to simplify user extraction from request. Remove type annotation for JwtPayload and clarify comments regarding JWT payload handling.

--- api-gateway/src/common/decorators/current-user.decorator.ts
+++ api-gateway/src/common/decorators/current-user.decorator.ts
@@ -1,11 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
-import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext): JwtPayload => {
+  (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest<Request>();
-    const userFromRequest = request.user as JwtPayload; // quì dentro c'è il Payload decodificato passato dalla funzione validate(...) della classe JwtStrategy
+    const userFromRequest = request.user; // quì dentro c'è il Payload decodificato dei JWT passato dal metodo validate(...) delle classi PassportStrategy dedicate ai JWT
     return userFromRequest;
   },
 );
